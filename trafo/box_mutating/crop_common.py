f5f38707f41f97af38420eb0962c8b89854c8548
Leon <89131603+advaml@users.noreply.github.com>
2021-09-02 19:47:09 +0200

    refactor crop Trafos

---
+++ trafo/box_mutating/crop_common.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from utils.bounding_boxes import BoundingBoxes
+
+
+def crop_image(self, img: np.ndarray, left_crop, right_crop, top_crop, bottom_crop) \
+        -> np.ndarray:
+    return img[top_crop:bottom_crop, left_crop:right_crop]
+
+
+def crop_boxes(self, boxes: BoundingBoxes, left_crop, right_crop, top_crop, bottom_crop) \
+        -> BoundingBoxes:
+    boxes[:, 0] -= left_crop  # x-left_crop
+    boxes[:, 1] -= top_crop  # y-top_crop
+    # Cut boxes if necessary
+    # Modify width and length if the bounding boxes are cutted at top or left
+    boxes[:, 2] += np.minimum(boxes[:, 0], 0)
+    boxes[:, 3] += np.minimum(boxes[:, 1], 0)
+    # Modify x, y if bounding boxes are cutted at top or left
+    boxes[:, :2] = np.maximum(boxes[:, :2], 0)
+    # Modify width and length if the bounding boxes are cutted at bottom or right
+    boxes[:, 2] = np.minimum(boxes[:, 2], right_crop - left_crop - boxes[:, 0])
+    boxes[:, 3] = np.minimum(boxes[:, 3], bottom_crop - top_crop - boxes[:, 1])
+    # Identify bounding boxes as zero-rows if they are not in the image
+    rows_to_zero = np.array(np.prod(boxes[:, 2:] <= 0, axis=1), dtype=bool)
+    boxes[rows_to_zero, :] = 0
+    return boxes
