c9d90c4305f416af3b217da1f45def7d932e8a1c
Leon <89131603+advaml@users.noreply.github.com>
2021-09-03 11:51:40 +0200

    introduce Color0ToMax transformation

--- trafo/color/__init__.py
+++ trafo/color/__init__.py
@@ -1,2 +1,3 @@
+from .color_0_to_max import Color0ToMax
 from .to_8_bit_color import To8BitColor
 from .truncate_gray_values import TruncateGrayValues
---
+++ trafo/color/color_0_to_max.py
@@ -0,0 +1,28 @@
+import torch
+import numpy as np
+from dataclasses import dataclass
+
+from ..trafo import Trafo
+from ..rules import preserve_bounding_boxes
+
+
+@preserve_bounding_boxes
+@dataclass
+class Color0ToMax(Trafo):
+    """Scale the pixel values of an image to the range [0, 255]."""
+
+    max: float
+
+
+@Color0ToMax.transform.register
+def _(self, image: np.ndarray) -> np.ndarray:
+    image -= image.min()
+    image = (np.maximum(image, 0) / image.max()) * self.max
+    return image
+
+
+@Color0ToMax.transform.register
+def _(self, image: torch.Tensor) -> torch.Tensor:
+    image -= image.min()
+    image = (np.maximum(image, 0) / image.max()) * self.max
+    return image
