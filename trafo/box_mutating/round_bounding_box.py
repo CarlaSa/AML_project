5b79afb5cca814244546d08e7e6c406c33675eab
christoph021198 <christoph021198@gmail.com>
2021-09-03 12:56:32 +0200

    minor changes for plots

--- trafo/box_mutating/round_bounding_box.py
+++ trafo/box_mutating/round_bounding_box.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 
 from ..trafo import Trafo
 
@@ -13,4 +13,4 @@ class RoundBoundingBox(Trafo):
 def _(self, boxes: np.ndarray) -> np.ndarray:
     boxes[:, :2] = np.floor(boxes[:, :2])
     boxes[:, 2:] = np.ceil(boxes[:, 2:])
-    return boxes
+    return boxes.astype('int')
