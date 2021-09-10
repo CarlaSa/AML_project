fb523d8e975df0f0177371d04022823daa63f07f
Leon <89131603+advaml@users.noreply.github.com>
2021-09-10 16:47:43 +0200

    fix imports from .torchvision

--- trafo/randomize/__init__.py
+++ trafo/randomize/__init__.py
@@ -1,3 +1,4 @@
 from .default_augmentation import default_augmentation
 from .randomize import randomize
-from torchvision import Autocontrast, AdjustSharpness, AdjustBrightness
+from .torchvision import Autocontrast, AdjustSharpness, AdjustBrightness, \
+    GaussianBlur, Rotate, Scale, Shear, Translate
