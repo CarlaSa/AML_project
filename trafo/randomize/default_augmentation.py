c64dd71dbe43bed8ff80d2f66c03ce85ae2ebeda
Leon <89131603+advaml@users.noreply.github.com>
2021-09-11 14:26:31 +0200

    add partial augmentation pipelines

--- trafo/randomize/default_augmentation.py
+++ trafo/randomize/default_augmentation.py
@@ -22,3 +22,21 @@ default_augmentation = Compose(
     Translate(translate_x=(0, 4), translate_y=(0, 4)),
     Scale(scale_factor=(1, 0.075), random_function=positive_half_gauss),
 )
+
+default_augmentation_only_values = Compose(
+    AdjustSharpness(sharpness_factor=(1, 0.5)),
+    AdjustBrightness(brightness_factor=(1, 0.05)),
+    GaussianBlur(x=(0, 1), p=(0.1, 0.1), random_function=random.uniform),
+    Autocontrast(),
+    BoundingBoxesToMask(),
+    NDArrayTo3dTensor(),
+)
+
+default_augmentation_only_geometric = Compose(
+    BoundingBoxesToMask(),
+    NDArrayTo3dTensor(),
+    Rotate(angle=(0, 3)),
+    Shear(shear_x=(0, 3), shear_y=(0, 3)),
+    Translate(translate_x=(0, 4), translate_y=(0, 4)),
+    Scale(scale_factor=(1, 0.075), random_function=positive_half_gauss),
+)
