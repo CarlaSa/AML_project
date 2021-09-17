60f49458a6b89a893cf216f23fc614ee38fd7538
Leon <89131603+advaml@users.noreply.github.com>
2021-09-17 22:47:36 +0200

    script to generate command lines for open Unet training jobs from table

---
+++ scripts/generate_train_unet_command.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+table = pd.read_csv('artifacts/unet_training_runs.csv',
+                    keep_default_na=False, na_values=[''])
+for index, row in table[table["dir"].isna()].iterrows():
+    command = (["--do-batch-norm"] if row.do_batch_norm == "TRUE" else []) + [
+        f"--batch-size={row.batch_size}",
+        f"--n-blocks={row.n_blocks}",
+        f"--n-initial-block-channels={row.n_initial_block_channels}",
+        f"--learning-rate={row.learning_rate}",
+        str(row.criterion),
+        str(row.augmentation)
+    ]
+    job_name = f"{row.criterion}_{row.augmentation}_specials"
+    arg_line = (f"--job-name={job_name} ../lslurm/run_gpu.sh "
+                + "python3 -m scripts.train_unet " + " ".join(command))
+    print(arg_line)
