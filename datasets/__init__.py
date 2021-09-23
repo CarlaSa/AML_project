2ec2cd0efb29b3f1be787de1d34fb57572478821
Leon <89131603+advaml@users.noreply.github.com>
2021-09-23 23:57:37 +0200

    add TestData(Dataset)

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,3 +3,4 @@
 from .io import save_dataset, LoadDataset
 from .custom_output import CustomOutput
 from .knit import Knit
+from .test_data import TestData
---
+++ datasets/test_data.py
@@ -0,0 +1,28 @@
+import os
+import torch
+from torchvision import io
+from typing import List, Tuple
+
+from torch.utils.data import Dataset
+
+
+class TestData(Dataset):
+    """Dataset featuring ids versus image tensor."""
+
+    image_dir: str
+    filenames: List[str]
+
+    def __init__(self, image_dir):
+        self.image_dir = str
+        self.filenames = [fn for fn in os.listdir() if fn.endswith(".png")]
+
+    def __len__(self) -> int:
+        return len(self.filenames)
+
+    def __getitem__(self, index) -> Tuple[torch.Tensor, str, str]:
+        filename = self.filenames[index]
+        path = os.path.join(self.image_dir, filename)
+        study_id, image_id = filename.split(".")[0].split("_")
+        assert len(study_id) == 12 and len(image_id) == 12
+        tensor = io.read_image(path, io.ImageReadMode.GRAY).to(float)/255
+        return tensor, study_id, image_id
