4d724d02365e7b6263de6b4f3569858e1c8d0d4a
Leon <89131603+advaml@users.noreply.github.com>
2021-09-24 00:03:47 +0200

    fix directory listing

--- datasets/test_data.py
+++ datasets/test_data.py
@@ -13,8 +13,9 @@ class TestData(Dataset):
     filenames: List[str]
 
     def __init__(self, image_dir):
-        self.image_dir = str
-        self.filenames = [fn for fn in os.listdir() if fn.endswith(".png")]
+        self.image_dir = image_dir
+        self.filenames = [fn for fn in os.listdir(image_dir)
+                          if fn.endswith(".png")]
 
     def __len__(self) -> int:
         return len(self.filenames)
