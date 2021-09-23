9413859ab4d97b55f823a9b11a5178c01eadc0b5
Carla Sagebiel <carlachristin@googlemail.com>
2021-09-23 20:26:59 +0200

    multimodal confusion matrix

---
+++ network/helper_functions.py
@@ -0,0 +1,22 @@
+import torch
+from sklearn.metrics import multilabel_confusion_matrix
+
+
+
+def get_multilabel_confusion_matrix(model, dataloader):
+    model.eval()
+    ylist =torch.zeros(0, device='cpu')
+    outlist =torch.zeros(0, device='cpu')
+    with torch.no_grad():
+        for x,y in dataloader:
+            x = x.to(device)
+            y = y.to(device)
+            x = x.float()
+            y = y.float()
+            out = model(x)
+            ylist =torch.cat([ylist,y.cpu()])
+            out = out >0.3
+            outlist=torch.cat([outlist,out.cpu()])
+    return multilabel_confusion_matrix(ylist, outlist)
+
+    
