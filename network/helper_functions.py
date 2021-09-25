b0190bbc5b57dc5b5799a7f7bb948b9a8b9210dd
Carla Sagebiel <carlachristin@googlemail.com>
2021-09-25 18:16:50 +0200

    confusion matrix

--- network/helper_functions.py
+++ network/helper_functions.py
@@ -1,11 +1,11 @@
 import torch
-from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import multilabel_confusion_matrix, confustion_matrix
 
 
 
 def get_multilabel_confusion_matrix(model, dataloader):
     model.eval()
-    device = "cuda" if torch.cuda.is_available() else "gpu"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     ylist =torch.zeros(0, device='cpu')
     outlist =torch.zeros(0, device='cpu')
     with torch.no_grad():
@@ -19,4 +19,22 @@ def get_multilabel_confusion_matrix(model, dataloader):
             out = out >0.3
             outlist=torch.cat([outlist,out.cpu()])
     return multilabel_confusion_matrix(ylist, outlist)
+
+
+def get_confusion_matrix(model, dataloader):
+    model.eval()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    ylist =torch.zeros(0, device='cpu')
+    outlist =torch.zeros(0, device='cpu')
+    with torch.no_grad():
+        for x,y in dataloader_val:
+            x = x.to(device)
+            y = y.to(device)
+            x = x.float()
+            y = y.float().argmax(dim = 1)
+            out = model(x)
+            ylist =torch.cat([ylist,y.cpu()])
+            out = torch.argmax(out, dim = 1)
+            outlist=torch.cat([outlist,out.cpu()])
+    return confustion_matrix(ylist, outlist)
 
